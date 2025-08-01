9a4ef572c977241f446c5814d64ca5da547bd826
HexaVortex <124194230+sakibsami995@users.noreply.github.com>
2025-08-01 21:32:41 +0600

    Changes made by Akash

--- problem02.c
+++ problem02.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 int main() {
+
+
+double green_wavelength = 530.0;
+    double green_angle = 65.0;
+    int green_order = 3;
+    double red_wavelength = 700.0;
+    int red_order = 2;
+
 
     return 0;
 }
